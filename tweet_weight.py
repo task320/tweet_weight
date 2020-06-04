f4854ca43ed8d65273976367e753f57df5b28f88
tanuki <task320@gmail.com>
2020-06-04 20:39:56 +0900

    ツィートOK

--- tweet_weight.py
+++ tweet_weight.py
@@ -2,8 +2,9 @@ import twitter
 from twitter.error import TwitterError
 import psycopg2
 import os
+import sys
 from datetime import date
-"""
+
 try:
     twitter_consumer_key = os.environ['TWITTER_CONSUMER_KEY'] 
     twitter_consumer_secret = os.environ['TWITTER_CONSUMER_SECRET'] 
@@ -16,28 +17,29 @@ try:
     pg_password = os.environ['PG_WEIGHTER_PASSWORD'] 
 
     dt = None
-    user_id = int(sys.argv[0])
+    user_id = sys.argv[1]
 
-    if len(sys.argv) >= 1:
-        dt = sys.argv[1]
-    else
+    if len(sys.argv) >= 3:
+        dt = sys.argv[2]
+    else:
         dt = date.today().isoformat()
 
     conn = psycopg2.connect("dbname='{0}' user='{1}' host='{2}' password='{3}'".format(pg_dbname, pg_user, pg_host, pg_password))
     cur = conn.cursor()    
-    cur.execute("select weight from weight_data where id = %s and insert_date = %s;", (user_id, dt))
+    cur.execute("select weight from weight_data where id = %s and to_char(insert_date, 'YYYY-MM-dd') = %s;", (user_id, dt))
     row = cur.fetchone()
-    if row is None
+    if row is None:
         raise Exception('体重データが取得できませんでした。')
 
     weight = row[0]
-
+ 
     api = twitter.Api(consumer_key=twitter_consumer_key,
                     consumer_secret=twitter_consumer_secret,
                     access_token_key=twitter_access_token_key,
                     access_token_secret=twitter_access_token_secret)
 
     status = api.PostUpdates('{0} :{1}Kg'.format(dt, weight))
+   
 except KeyError as e:
     print('KeyError')
     print(e)
@@ -55,4 +57,3 @@ except Exception as e:
 
 print('Success to tweet!!')
 print(status)
-"""
